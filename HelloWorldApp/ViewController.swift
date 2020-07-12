b7b966db0f289a625fa723105c53f4cc27437ff2
Govorov Erx <govoroverx@icloud.com>
2020-07-12 20:22:03 +0300

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Говоров Эрхаан on 12.07.2020.
+//  Copyright © 2020 Govorov Erkhaan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
